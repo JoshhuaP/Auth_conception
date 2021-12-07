a8306e0981555e181bcea8a39756cab09aa45b2a
Costiou Rozenn <rozenn.costiou@orange.fr>
2021-12-07 11:29:09 +0100

    correction des requette

--- auth/RequestReader/RequestReaderUser.js
+++ auth/RequestReader/RequestReaderUser.js
@@ -1,19 +1,18 @@
 
-class RequestReaderRegister {
+module.exports = class RequestReaderUser {
 
     readData(request){
-        //get the information in the request
-        info = request.query;
-        mail = info.mail;
-        pseudo = info.pseudo;
-        pasword = info.pwd;
-
-        info = {}
+        let body = request.body;
+        let mail = body.mail;
+        let pseudo = body.pseudo;
+        let pasword = body.pwd;
+        let info = {}
         if(mail == null || pseudo == null || pasword == null){
             info.status = 400;
             info.body = {message: 'accept : mail=test@test.fr&pseudo=test&pwd=testmdp'};
             return info;
         }
+        /*
         //request the controleur
         result = addUserToBD(mail, pseudo, password);
         // 201 -> created return id
@@ -29,6 +28,10 @@ class RequestReaderRegister {
             info.status = 500;
             info.body = {message: 'restart after'};
         }  
+        */
+        info.status = 201;
+        info.body = {id: "idtest"};
+
         return info;
     }
          
--- auth/routes/users.js
+++ auth/routes/users.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+const RequestReaderUser  = require('../RequestReader/RequestReaderUser');
 
 /* POST registri for add user. */
 router.post('/',(request,res) => {
-    info = RequestReaderRegister.readData(request);
+    console.log("rentre debut")
+    let requestReaderUser = new RequestReaderUser();
+    console.log("apres")
+    info = requestReaderUser.readData(request);
     res.status(info.status).json(info.body).send();
     });
 module.exports = router;
