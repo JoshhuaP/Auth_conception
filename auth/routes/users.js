3ca1b9a8faada9466e57c6934f47034223462500
Costiou Rozenn <rozenn.costiou@orange.fr>
2021-12-07 10:19:06 +0100

    feat : changement de register par user

--- auth/routes/users.js
+++ auth/routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
+/* POST registri for add user. */
+router.post('/',(request,res) => {
+    info = RequestReaderRegister.readData(request);
+    res.status(info.status).json(info.body).send();
+    });
 module.exports = router;
