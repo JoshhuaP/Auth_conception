6849577303280edf703e5c7953f624af5bce9f54
Fabien Verspan <78422154+fab971cool@users.noreply.github.com>
2021-12-07 10:23:20 +0100

    chose : ajout d'un connector pour la bdd

--- auth/routes/add_user.js
+++ auth/routes/add_user.js
@@ -1,49 +1,27 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) 
-{
-  res.send('add a user');
-    addUserToDB("tribu@squad.auth", "auth", "mdp");
-
-
-
-});
-
-const {Sequelize} = require('sequelize');
 const bcrypt = require('bcrypt');
 
 
-// Only string variables
-function addUserToDB(mail, pseudo, password)
+/**
+ * 
+ * @param { adresse mail de l'utilisateur } mail 
+ * @param { le pseudo de l'utilisateur } pseudo 
+ * @param { le mot de passe en claire } password 
+ * @param { un connecteur de la base de donnnée } connnector 
+ */
+function addUserToDB(mail, pseudo, password, connnector)
 {
     const saltRounds = 10;
-    // Connexion à la BDD
-    const sequelize = new Sequelize("", "root", "toor",
-        { dialect : "mysql", host : "localhost"});
 
-    // vérif de connexion
-    try {
-        sequelize.authenticate();
-        console.log(" Database connected");
-        let saltString = start.toString();
-        
-
-        bcrypt.genSalt(saltRounds, function(err, salt) {
-            bcrypt.hash(password, salt, (err, hashPasswd) => {
-                if (!err)
-                {
-                    console.log(saltString);
-                    // query to add a user into database
-                    //sequelize.query("INSERT User VALUES(NULL, %s, %s, %s);", mail, pseudo, hashPasswd);
-                }
-            });
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(password, salt, (err, hashPasswd) => {
+            if (!err)
+            {
+                console.log(saltString);
+                // requete ppour ajouter un utilisateur dans la bdd
+                connector.query("INSERT User VALUES(NULL, %s, %s, %s);", mail, pseudo, hashPasswd);
+            }
         });
-
-    } catch(e){
-        console.error(" Can't connect to database : \n", e.message );
-    }
+    });
 }
 
 
