b41c66284eff5844999d24010f97e0370e2fa5e9
Costiou Rozenn <rozenn.costiou@orange.fr>
2021-12-08 14:29:32 +0100

    fix : supression du drop

--- auth_db/sqlite_connection.js
+++ auth_db/sqlite_connection.js
@@ -26,7 +26,6 @@ let db = new sqlite3.Database(__dirname +"/db_conception.db", sqlite3.OPEN_READW
       return console.error(err.message);
     }
 
-    db.run('DROP TABLE `user`');
     db.run('CREATE TABLE IF NOT EXISTS `user` (`id_user` INTEGER PRIMARY KEY AUTOINCREMENT , `pseudo` varchar(50) NOT NULL,`email` varchar(100) NOT NULL,`password` varchar(250) NOT NULL)');
     db.run('CREATE TABLE IF NOT EXISTS `battle` ( `id_battle` INTEGER PRIMARY KEY AUTOINCREMENT , `id_joueur1` INTEGER NOT NULL, `id_joueur2` INTEGER NOT NULL, `score1` smallint(6) NOT NULL, `score2` smallint(6) NOT NULL, `date` datetime NOT NULL, `duree` time NOT NULL, CONSTRAINT `FK1_battle_score` FOREIGN KEY (`id_joueur1`) REFERENCES `user` (`id_user`), CONSTRAINT `FK2_battle_score` FOREIGN KEY (`id_joueur2`) REFERENCES `user` (`id_user`))')
     console.log('Connected to the in-memory SQlite database.');
