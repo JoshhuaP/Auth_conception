faf0c24394cac641791629fb722ec9e28766b831
Fabien Verspan <78422154+fab971cool@users.noreply.github.com>
2021-12-08 11:47:20 +0100

    Change user.js to add_user and delete user fixed

--- auth/controler/delete_user.js
+++ auth/controler/delete_user.js
@@ -2,33 +2,9 @@ var user_dao = require('auth_db').user_dao;
 
 module.exports = class ControleurUser {
     /**
-     * @param { Cle d'identification } key 
+     * @param { adresse mail de l'utilisateur } mail 
+     * @param { fonction de callback } callback 
      */
-
-    deleteUserToDB(mail)
-    {
-        user_dao.findByMail(mail, function(user){
-            key = user.id;
-            user_dao.findByKey(key,function(err,rows){
-                if(rows.toString()==""){
-                    user_dao.delete(key, (err, user)=>{
-                        if(err == null){
-                            console.log(user)
-                            return {status:201, id : user.id}
-                        }else{
-                            return {"erreur" : "bdd"}
-                        }
-                    });
-                }else{
-                    console.log(rows)
-                    return {status:403, id : "delete"}
-                }
-            });
-        } );
-
-    }
-}
-
 
 deleteUserToDB(mail, callback)
 {
