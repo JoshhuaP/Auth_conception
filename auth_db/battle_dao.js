9206ae237f32399f208a53a9c7f14ecf2a5511dc
Costiou Rozenn <rozenn.costiou@orange.fr>
2021-12-08 10:03:41 +0100

    correction dao de battle

--- auth_db/battle_dao.js
+++ auth_db/battle_dao.js
@@ -1,14 +1,19 @@
 var db = require('./sqlite_connection');
 var BattleDao = function(){
-
+ 
     /**
      * values : Tableau de valeur a inserer
      * callback : Message d'erreur
      */
-    this.insert = function(values, callback){
-        let stmt = db.prepare("INSERT INTO battle VALUES(?,?,?,?,?,?,?)");
-        stmt.run([values[0], values[1], values[2], values[3], values[4], values[5], values[6]], callback);
-    }; 
+     this.insert = function(values, callback){
+        db.query("INSERT INTO battle (id_joueur1, id_joueur2, score1, score2, date, duree) VALUES (\"" + values[0] + "\", \"" + values[1] + "\", \"" + values[2] +"\", \"" + values[3]+"\", \"" + values[4]+"\", \"" + values[5] +"\")",function(err,rows){
+            if(err){
+                callback(err, null);
+            }else{
+                callback(null, rows);
+            }
+        });
+    };
 
     /**
      * key : Cle d'identification
@@ -25,14 +30,14 @@ var BattleDao = function(){
      * callback : Message d'erreur
      */
      this.delete = function(key, callback){
-        db.run("DELETE FROM battle WHERE id_battle=?",key,callback);
+        db.query("DELETE FROM battle WHERE id_battle=?",key,callback);
     };
 
     /**
      * callback : Message d'erreur
      */
      this.findAll = function(callback){
-        db.getAll("SELECT * FROM battle",callback);
+        db.query("SELECT * FROM battle",callback);
     };
 
     /**
@@ -40,7 +45,7 @@ var BattleDao = function(){
      * callback : Message d'erreur
      */
 	this.findByKey = function(key, callback){
-        db.all("SELECT * FROM battle WHERE id_battle =?",key, function(err,rows){
+        db.query("SELECT * FROM battle WHERE id_battle =?",key, function(err,rows){
             if(err){
                 console.log(err.message);
             }else{
@@ -50,19 +55,19 @@ var BattleDao = function(){
     };
 
     /**
-     * key : Cle d'identification
+     * key : Cle d'identification du joueur 1
      * callback : Message d'erreur
      */
     this.findByPlayerOneForeignKey = function(key, callback){
-        db.getAll("SELECT * FROM battle WHERE joueur1 =?",key, callback);
+        db.query("SELECT * FROM battle WHERE joueur1 =?",key, callback);
     };
 
     /**
-     * key : Cle d'identification
+     * key : Cle d'identification du joueur 1
      * callback : Message d'erreur
      */
      this.findByPlayerTwoForeignKey = function(key, callback){
-        db.getAll("SELECT * FROM battle WHERE joueur2 =?",key, callback);
+        db.query("SELECT * FROM battle WHERE joueur2 =?",key, callback);
     };
 };
 
