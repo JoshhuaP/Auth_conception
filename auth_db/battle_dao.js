571506e6a5e130b50969cca5ead54f59f7d7e6fa
JoshhuaP <Joshua.pinneau@gmail.com>
2021-12-08 11:56:13 +0100

    Add BDD

--- auth_db/battle_dao.js
+++ auth_db/battle_dao.js
@@ -5,10 +5,16 @@ var BattleDao = function(){
      * values : Tableau de valeur a inserer
      * callback : Message d'erreur
      */
+
     this.insert = function(values, callback){
-        let stmt = db.prepare("INSERT INTO battle VALUES(?,?,?,?,?,?,?)");
-        stmt.run([values[0], values[1], values[2], values[3], values[4], values[5], values[6]], callback);
-    }; 
+        db.query("INSERT INTO user (id_joueur1, id_joueur2, score1, score1, date, duree) VALUES (\"" + values[0] + "\", \"" + values[1] + "\", \"" + values[2] +"\""+"\", \"" + values[3] +"\""+"\", \"" + values[4] +"\""+"\", \"" + values[5] +"\")",function(err,rows){
+            if(err){
+                callback(err, null);
+            }else{
+                callback(null, rows);
+            }
+        });
+    };
 
     /**
      * key : Cle d'identification
@@ -25,14 +31,14 @@ var BattleDao = function(){
      * callback : Message d'erreur
      */
      this.delete = function(key, callback){
-        db.run("DELETE FROM battle WHERE id_battle=?",key,callback);
+        db.run("DELETE FROM battle WHERE id_battle=?",key, callback);
     };
 
     /**
      * callback : Message d'erreur
      */
      this.findAll = function(callback){
-        db.getAll("SELECT * FROM battle",callback);
+        db.getAll("SELECT * FROM battle", callback);
     };
 
     /**
@@ -40,13 +46,7 @@ var BattleDao = function(){
      * callback : Message d'erreur
      */
 	this.findByKey = function(key, callback){
-        db.all("SELECT * FROM battle WHERE id_battle =?",key, function(err,rows){
-            if(err){
-                console.log(err.message);
-            }else{
-                callback(rows);
-            }
-        });
+        db.all("SELECT * FROM battle WHERE id_battle =?",key, callback)
     };
 
     /**
