939649ba8323229025fdada633779f9d059eea2a
benjamin <benjaminh@live.fr>
2021-12-09 13:16:41 +0100

    Delete the logs that crash the tests

--- auth_db/sqlite_connection.js
+++ auth_db/sqlite_connection.js
@@ -6,7 +6,7 @@ let db = new sqlite3.Database(__dirname +"/db_conception.db", sqlite3.OPEN_READW
 
     db.run('CREATE TABLE IF NOT EXISTS `user` (`id_user` INTEGER PRIMARY KEY AUTOINCREMENT , `pseudo` varchar(50) NOT NULL,`email` varchar(100) NOT NULL,`password` varchar(250) NOT NULL)');
     db.run('CREATE TABLE IF NOT EXISTS `battle` ( `id_battle` INTEGER PRIMARY KEY AUTOINCREMENT , `id_joueur1` INTEGER NOT NULL, `id_joueur2` INTEGER NOT NULL, `score1` smallint(6) NOT NULL, `score2` smallint(6) NOT NULL, `date` datetime NOT NULL, `duree` time NOT NULL, CONSTRAINT `FK1_battle_score` FOREIGN KEY (`id_joueur1`) REFERENCES `user` (`id_user`), CONSTRAINT `FK2_battle_score` FOREIGN KEY (`id_joueur2`) REFERENCES `user` (`id_user`))')
-    console.log('Connected to the in-memory SQlite database.');
+    //console.log('Connected to the in-memory SQlite database.');
   });
 
 module.exports = db
